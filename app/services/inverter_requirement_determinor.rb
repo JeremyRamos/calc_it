816b54c13dde89959644d883daef71da0fd7586c
jeremy <jeremyramos92@gmail.com>
2023-03-02 21:51:11 +0200

    show hours and minutes

---
+++ app/services/inverter_requirement_determinor.rb
@@ -0,0 +1,35 @@
+module InverterRequirementDeterminor
+  extend self
+
+  BATTERY_CAPACITY = 52 # calculations is done on a 105 Ah battery, only using 50%
+
+  def call(total_wattage)
+    return [0, 0, 0] if total_wattage.to_s.start_with?('0')
+
+    [calculate_inverter_size(total_wattage), *calculate_battery_duration(total_wattage)]
+  end
+
+  private
+
+  def calculate_inverter_size(total_wattage)
+    inverter_size = total_wattage.to_f + (total_wattage.to_f * 0.20)
+    inverter_size.ceil
+  end
+
+  def calculate_battery_duration(total_wattage, efficiency = 0.8, voltage = 12)
+    # Assuming an 80% efficiency rate. An 80% efficient inverter will use (watts / 0.8) W to produce total_wattage.
+    power_required = total_wattage.to_f / efficiency
+
+    # this is the ampere a 12 volt battery will require to produce the power_required
+    amp_hours = power_required / voltage
+
+    # how long the size batter will provide power
+    duration_in_hours = BATTERY_CAPACITY / amp_hours
+
+    # get hours and minutes from hours
+    hours, minutes = duration_in_hours.divmod(1)
+    minutes = (minutes * 60).to_i
+
+    [hours, minutes]
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module InverterCalculation
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths += %W(#{config.root}/app/*)
   end
 end
