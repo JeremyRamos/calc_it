529b039a1cde6e3066a2860236de73415d42fecf
jeremy <jeremyramos92@gmail.com>
2023-03-05 17:55:27 +0200

    add debounce and debounce searches by 300ms

--- app/javascript/controllers/calculations_controller.js
+++ app/javascript/controllers/calculations_controller.js
@@ -1,7 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
+import debounce from "debounce";
 
 export default class extends Controller {
 
+  initialize() {
+    this.calculate = debounce(this.calculate.bind(this), 300);
+  }
   calculate() {
     this.element.requestSubmit();
   }
