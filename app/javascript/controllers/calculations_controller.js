df3016f627031602e0ecafda7e0d3dbe3d1ae3a3
jeremy <jeremyramos92@gmail.com>
2023-03-05 17:45:57 +0200

    clean up

--- app/javascript/controllers/calculations_controller.js
+++ app/javascript/controllers/calculations_controller.js
@@ -1,9 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['submit']
 
   calculate() {
-    this.submitTarget.click()
+    this.element.requestSubmit();
   }
 }
