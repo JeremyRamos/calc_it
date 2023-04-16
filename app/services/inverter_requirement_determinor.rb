3fda1f65df402e62f7c91da9a7b82fe349e69b4c
jeremy <jeremyramos92@gmail.com>
2023-04-16 22:08:37 +0200

    provide battery size

--- app/services/inverter_requirement_determinor.rb
+++ app/services/inverter_requirement_determinor.rb
@@ -1,12 +1,10 @@
 module InverterRequirementDeterminor
   extend self
 
-  BATTERY_CAPACITY = 52 # calculations is done on a 105 Ah battery, only using 50%
-
-  def call(total_wattage)
+  def call(total_wattage, battery_size)
     return [0, 0, 0] if total_wattage.to_s.start_with?('0')
 
-    [calculate_inverter_size(total_wattage), *calculate_battery_duration(total_wattage)]
+    [calculate_inverter_size(total_wattage), *calculate_battery_duration(total_wattage, battery_size.to_i)]
   end
 
   private
@@ -16,7 +14,7 @@ module InverterRequirementDeterminor
     inverter_size.ceil
   end
 
-  def calculate_battery_duration(total_wattage, efficiency = 0.8, voltage = 12)
+  def calculate_battery_duration(total_wattage, battery_size = 105, efficiency = 0.8, voltage = 12)
     # Assuming an 80% efficiency rate. An 80% efficient inverter will use (watts / 0.8) W to produce total_wattage.
     power_required = total_wattage.to_f / efficiency
 
@@ -24,7 +22,7 @@ module InverterRequirementDeterminor
     amp_hours = power_required / voltage
 
     # how long the size batter will provide power
-    duration_in_hours = BATTERY_CAPACITY / amp_hours
+    duration_in_hours = ((battery_size / 2) / amp_hours)
 
     # get hours and minutes from hours
     hours, minutes = duration_in_hours.divmod(1)
