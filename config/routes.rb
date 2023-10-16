e1466801c98a7bdd467ae3bd3fb086175dbb4f66
jeremy <jeremyramos92@gmail.com>
2023-10-16 21:50:12 +0200

    namespace inverters calculation

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'calculations#new'
+  root 'inverters/calculations#new'
 
-  resources :calculations, only: [:new, :create] do
-    post :calculate, on: :collection
-  end
+  # resources :calculations, only: [:new, :create] do
+  #   post :calculate, on: :collection
+  # end
+  
+  post '/calculate', to: 'inverters/calculations#calculate'
   
   get '/units_calculation', to: 'units/calculations#new'
-  post '/units_calculation', to: 'units/calculations#calculate'
+  post '/calculate', to: 'units/calculations#calculate'
 end
