71f2241c5f1b5f9c77194c432eacd42d70c1da0c
jeremy <jeremyramos92@gmail.com>
2024-09-10 12:41:35 +0200

    rename name of app

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require 'active_support/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module InverterCalculation
+module CalcIt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
